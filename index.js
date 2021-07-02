f3ba64812a31848c56e0306b585306dfbd9a0ace
cristina <cristinaxavierprodutora.gmail.com>
2021-07-02 02:27:54 -0300

    jsexercicio

---
+++ index.js
@@ -0,0 +1,36 @@
+
+function bomDia() {
+  console.log('bom dia!');
+  }
+  bomDia();
+
+  function meuNome(name) {
+  console.log('Meu nome é  ${name}');
+  }
+  meuNome('Cristina');
+  
+  function dados(){
+  console.log('nome:"Cristina", idade:"51" , estilo musical preferido: "Anos 80"')
+              }
+   dados();
+  
+  function filme(){
+    console.log( "Em algum lugar do passado" )
+  }
+  filme();
+  
+   function triplo(x) {
+   return x*3
+  }
+  triplo(17)
+  console.log(triplo(17))
+   //1 - crie uma função que exiba uma mensagem no console
+
+//2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+
+//3 - crie uma função que receba seu nome, idade, e estilo musical preferido (parâmetros) e exiba no console
+
+//4 - crie uma função que receba o seu filme favorito, musica favorita (parâmetros) e exiba no console
+
+//5 - crie uma função que retorne o triplo do número recebido no parâmetro da função
+
